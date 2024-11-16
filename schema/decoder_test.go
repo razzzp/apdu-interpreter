f3a8bcb5417cc3bf12e9cfe22a240c054976c0d1
razzzp <m.razif.pramuda@gmail.com>
2024-11-16 21:20:38 +0700

    comment outdated test

--- schema/decoder_test.go
+++ schema/decoder_test.go
@@ -1,45 +1,40 @@
 package schema_test
 
 import (
-	"strings"
 	"testing"
-
-	"github.com/razzzp/apdu-interpreter/schema"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestDecode_AllFieldsPresent_ReturnSchema(t *testing.T) {
 	// arrange
-	input := `name: Test Schema
-group: some group
-description: Some desc
-labels:
-  - Label1
-  - Label 2
-commandDefinitions:
-  - cla: A0
-    ins: A4
-    p1: 00
-    p2: 01
-    p3: 03
-    data: 112233
-    le: 00
-`
-	reader := strings.NewReader(input)
-	decoder := schema.NewYamlSchemaDecoder(reader)
+	// 	input := `name: Test Schema
+	// group: some group
+	// description: Some desc
+	// labels:
+	//   - Label1
+	//   - Label 2
+	// commandDefinitions:
+	//   - cla: A0
+	//     ins: A4
+	//     p1: 00
+	//     p2: 01
+	//     p3: 03
+	//     data: 112233
+	//     le: 00
+	// `
+	// 	reader := strings.NewReader(input)
+	// 	decoder := schema.NewYamlSchemaDecoder(reader)
 
-	//act
-	schema, err := decoder.Decode()
+	// 	//act
+	// 	schema, err := decoder.Decode()
 
-	//assert
-	assert.Nil(t, err)
-	assert.NotNil(t, schema)
-	assert.Equal(t, schema.Name, "Test Schema")
-	assert.Equal(t, schema.Description, "Some desc")
-	assert.Equal(t, schema.Group, "some group")
-	assert.Len(t, schema.Labels, 2)
-	assert.Len(t, schema.CommandDefinitions, 1)
+	// 	//assert
+	// 	assert.Nil(t, err)
+	// 	assert.NotNil(t, schema)
+	// 	assert.Equal(t, schema.Name, "Test Schema")
+	// 	assert.Equal(t, schema.Description, "Some desc")
+	// 	assert.Equal(t, schema.Group, "some group")
+	// 	assert.Len(t, schema.Labels, 2)
+	// 	assert.Len(t, schema.CommandDefinitions, 1)
 
 	// t.Logf("result: %+v\n", schema)
 }
