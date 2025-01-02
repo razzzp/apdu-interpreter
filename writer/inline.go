6dca5d2276cab6ef0d53da5f757034d52b3624b8
muhampra <muhammadrazif.pramuda@idemia.com>
2025-01-02 16:43:47 +0700

    wip writer

--- writer/inline.go
+++ writer/inline.go
@@ -1,18 +1,46 @@
 package formatter
 
 import (
-	"encoding/hex"
 	"io"
+	"log"
 
 	"github.com/razzzp/apdu-interpreter/interpreter"
 )
 
 type textInlineWriter struct {
+	CmdColWidth  int
+	IntpColWidth int
+	writer       io.StringWriter
+}
+
+func NewTextInlineWriter(cmdColWidth int, intpColWidth int, writer io.StringWriter) *textInlineWriter {
+	// ensure multiple of 3
+	for cmdColWidth%3 != 0 {
+		cmdColWidth++
+	}
+	for intpColWidth%3 != 0 {
+		intpColWidth++
+	}
+	return &textInlineWriter{
+		CmdColWidth:  cmdColWidth,
+		IntpColWidth: intpColWidth,
+		writer:       writer,
+	}
+}
+
+func (tf *textInlineWriter) formatBytes(bytes []byte) string {
+	// TODO
+	return ""
+}
+
+func (tf *textInlineWriter) writeInterpretation(interpretation *interpreter.ApduInterpretation) {
+	cmdAsHex := tf.formatBytes(interpretation.Command.Command.AsBytes())
+	// TODO
+	log.Print(cmdAsHex)
 }
 
 func (tf *textInlineWriter) Write(interpretations []*interpreter.ApduInterpretation, writer io.StringWriter) {
 	for _, intp := range interpretations {
-		cmdAsHex := hex.EncodeToString(intp.Command.Command.AsBytes())
-		writer.WriteString(cmdAsHex)
+		tf.writeInterpretation(intp)
 	}
 }
--- writer/interface.go
+++ writer/interface.go
@@ -1,11 +1,9 @@
 package formatter
 
 import (
-	"io"
-
 	"github.com/razzzp/apdu-interpreter/interpreter"
 )
 
 type InterpretationWriter interface {
-	Write(interpretations []*interpreter.ApduInterpretation, writer io.StringWriter)
+	Write(interpretations []*interpreter.ApduInterpretation)
 }
